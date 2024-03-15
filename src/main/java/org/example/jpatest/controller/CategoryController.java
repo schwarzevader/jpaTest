a1e5310b27c5e9d76760c38675feb215d0051bfb
vlad <vladislav0712@gmail.com>
2024-03-15 19:59:40 +0300

    commit in main

--- src/main/java/org/example/jpatest/controller/CategoryController.java
+++ src/main/java/org/example/jpatest/controller/CategoryController.java
@@ -24,7 +24,7 @@ public class CategoryController {
 
     @Autowired
     private ProductCategoryRepo productCategoryRepo;
-//some  janeg
+//some  janegyy
 
     @GetMapping
 //    public ResponseEntity<CustomResponse>getAllCategories(){
